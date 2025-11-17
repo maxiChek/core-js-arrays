8f7450decc23518912ad4e6d2088a16967dd4511
Maksim Grekov <mgrekov5@gmail.com>
2025-11-18 02:59:16 +0300

    feat: 12 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -394,7 +394,7 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(arr) {
+function getFalsyValuesCount(/* arr */) {
   throw new Error('Not implemented');
 }
 
